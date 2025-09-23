74ed8c69eb13d212a5505588196648c250890bda
kriztpow <45737457+kriztpow@users.noreply.github.com>
2025-09-22 22:23:11 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,4 @@ plugins {
     id("com.android.application") version "8.5.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
-dependencies {
-    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
-}
+
