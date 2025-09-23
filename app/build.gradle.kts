62f2bc9b7e4e2cc1ebfc33be3dae3d753b15f1da
kriztpow <45737457+kriztpow@users.noreply.github.com>
2025-09-22 23:30:57 -0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.tvbrowser"
+    namespace = "com.example.mindmirror"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.tvbrowser"
+        applicationId = "com.example.mindmirror"
         minSdk = 30
         targetSdk = 34
         versionCode = 1
